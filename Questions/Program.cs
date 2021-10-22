d9610e93da631e755e09cba90c978bdc9941f92f
Pheo <jeanmichmichlababine@yahoo.lol>
2021-10-22 18:05:07 +0200

    version 1.2

--- Questions/Program.cs
+++ Questions/Program.cs
@@ -7,26 +7,26 @@ namespace Questions
         static void Main(string[] args)
         {
             // Définie le nombre de questions
-            // Définie la note
+            // Définie la note (un point gagnée par réponses justes)
                         
             int NB_QUESTIONS = 5;
             int Note = 0;            
 
-            /* Avant de commencer la boucle avec Question n°"i", compare si "i" est supérieur aux nombre total de questions posées. 
-               A chaque itération de la boucle, augmente le numéro de la question de 1, et pose la prochaine question */
+            /* Avant de commencer la boucle avec Question n°"i", compare si "i" est supérieur aux nombre total de questions posées. Si "i" est supérieur, casse la boucle.
+               A chaque itération de la boucle, augmente le numéro de la question de 1, et affiche "Question n°"i" */
             int i = 0;
             for (; i < NB_QUESTIONS; i++)
             {
                 Console.WriteLine("Questions n°" + (i+1) + "/" + NB_QUESTIONS);
 
-                // Randomize une opération, attribue le résultat de a+b  à add et a*b à mul
+                // Randomize des nombres pour "a" et "b", déclare "c", lance un "pile ou face" pour décider quelle sera l'opération
 
                 Random rand = new Random();
                 int a = rand.Next(1, 51);
                 int b = rand.Next(2, 11);
                 int c = 0;
                 int op = rand.Next(0, 2);
-                // if génère une addition et attribue la réponse d'une addition à "c", else fait la même pour une multiplication
+                // if génère une addition entre "a" et "b" et attribue la réponse à "c", else fait la même pour une multiplication
                 if (op == 0)
                 {                    
                     Console.WriteLine(a.ToString() + " + " + b.ToString() + " = ");
@@ -39,17 +39,17 @@ namespace Questions
                 }
 
                 // Prend une réponse de l'utilisateur et vérifie s'il s'agit d'un nombre entier
-                              
-                String Entrée = Console.ReadLine();
+                                
+                    String Entrée = Console.ReadLine();
+                    int Réponse = 0;
+                    bool vérif = int.TryParse(Entrée, out Réponse);
+                    if (vérif == false)
 
-                int Réponse = 0;
-                bool vérif = int.TryParse(Entrée, out Réponse);
-                if (vérif == false)
-                {
+                    {
                         Console.WriteLine("Tu dois donner un nombre mdr");
-                }                
-
-                // Vérifie si la réponse est juste. Si oui, rajoute un point à la note.
+                    }
+                
+                // Vérifie si la réponse est juste (= à c). Si oui, rajoute un point à la note.
                 if (Réponse == c)
                 {
                     Note++;                    
